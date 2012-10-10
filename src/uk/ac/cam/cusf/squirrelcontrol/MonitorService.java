e50968e063d0e397e349880300709507b531b967
Edward Cunningham <edward.cunningham@gmail.com>
2012-10-10 17:36:55 +0100

    Commiting further updates

--- src/uk/ac/cam/cusf/squirrelcontrol/MonitorService.java
+++ src/uk/ac/cam/cusf/squirrelcontrol/MonitorService.java
@@ -1,5 +1,6 @@
 package uk.ac.cam.cusf.squirrelcontrol;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -8,7 +9,10 @@ import java.util.Map.Entry;
 import android.app.ActivityManager;
 import android.app.Service;
 import android.app.ActivityManager.RunningServiceInfo;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
@@ -17,12 +21,17 @@ public class MonitorService extends Service {
 
     public final static String TAG = "SquirrelControl";
 
+    public final static String SMS_RECEIVED = "uk.ac.cam.cusf.intent.SMS_RECEIVED";
+    
     private final static long DELAY = 30000;
 
     ActivityManager activityManager;
     HashMap<String, String> serviceMonitor;
 
     Handler handler;
+    
+    BroadcastReceiver aliveListener, commandListener;
+    long lastHeard = System.currentTimeMillis();
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -41,6 +50,50 @@ public class MonitorService extends Service {
                 SquirrelControl.LOG_ACTION);
         serviceMonitor.put(SquirrelControl.RADIO_SERVICE,
                 SquirrelControl.RADIO_ACTION);
+        
+        aliveListener = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals("uk.ac.cam.cusf.intent.ALIVE")) {
+                    lastHeard = System.currentTimeMillis();
+                    Log.i(TAG, "Alive");
+                }
+            }
+        };
+
+        IntentFilter actionChanged = new IntentFilter();
+        actionChanged.addAction("uk.ac.cam.cusf.intent.ALIVE");
+        registerReceiver(aliveListener, actionChanged);
+        
+        commandListener = new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String command = intent.getStringExtra("command");
+                if (command.equals("start")) {
+                    handler.postDelayed(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            SquirrelControl.enabledServices.put(SquirrelControl.LOG_SERVICE, true);
+                            SquirrelControl.enabledServices.put(SquirrelControl.RADIO_SERVICE, true);
+                            SquirrelControl.cameraStarted = true;
+                        }
+                        
+                    }, 2000);
+
+                } else if (command.equals("stop")) {
+                    SquirrelControl.enabledServices.put(SquirrelControl.LOG_SERVICE, false);
+                    SquirrelControl.enabledServices.put(SquirrelControl.RADIO_SERVICE, false);
+                    SquirrelControl.cameraStarted = false;
+                }
+            }
+            
+        };
+        
+        IntentFilter command = new IntentFilter();
+        command.addAction(SMS_RECEIVED);
+        registerReceiver(commandListener, command);
 
     };
 
@@ -79,6 +132,15 @@ public class MonitorService extends Service {
                 startService(intent);
             }
         }
+        
+        if (System.currentTimeMillis() - lastHeard > 5*60000 && SquirrelControl.enabledServices.get(SquirrelControl.RADIO_SERVICE)) {
+            try {
+                Log.i(TAG, "Rebooting");
+                Runtime.getRuntime().exec(new String[]{"/system/bin/su","-c","reboot now"});
+            } catch (IOException e) {
+                Log.i(TAG, "Failed to reboot");
+            }
+        }
 
     }
 
@@ -98,7 +160,8 @@ public class MonitorService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-
+        unregisterReceiver(aliveListener);
+        unregisterReceiver(commandListener);
     }
 
 }
